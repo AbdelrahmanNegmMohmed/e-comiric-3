6ebfacd5111b11c6c4608ac0fca945ac69590a5f
AbdelrahmanNegmMohamed <abdelrahman.negm.mohamed@gmail.com>
2024-02-01 06:57:30 +0200

    nave-bar-route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBAR/NavBar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Product from './Pages/Product';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
+    <div>
+        <BrowserRouter>
+            <NavBar/>
+            <Routes>
+              <Route path='/' element={<Shop/>}/>
+              <Route path='/mens' element={<ShopCategory   category="men"/>}/>
+              <Route path='/womens' element={<ShopCategory  category="women"/>}/>
+              <Route path='/kids' element={<ShopCategory   category="kid"/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='/login' element={<LoginSignup/>}/>
+              <Route path='/product' element={<Product/>}>
+                <Route path=':productId' element={<Product/>}/>
+              </Route>
+             </Routes>
+              </BrowserRouter>
   
     </div>
-  );
+  )
+  
 }
 
 export default App;
